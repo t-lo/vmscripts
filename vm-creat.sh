7b718a4a66ab26702a3e00bb1b362ce648fe270b
Thilo Fromm <github@thilo-fromm.de>
2015-10-31 17:57:11 +0100

    vm creat: use getopt
    Signed-off-by: Thilo Fromm <github@thilo-fromm.de>

--- vm-creat.sh
+++ vm-creat.sh
@@ -18,71 +18,98 @@
 #    along with vmscripts. If not, see <http://www.gnu.org/licenses/>.#
 #
 
-# the defaults
-def_disk="4G"
-def_mem="512M"
-def_cpu="1"
-def_net="172.16.10.0/24"
-def_forward_ports="22,25,80"
-
 
 vmscripts_prereq="name"
 
+# the options, including their respective defaults
+disk_size="4G"
+disk_image=""
+iso_image=""
+move=false
+mem="512M"
+cpus="1"
+net_mode="hidden" # hidden, tap
+net="172.16.10.0/24"
+forward_ports="22,25,80"
+
 creat_usage () {
     echo " Usage:"
-    echo -n "  vm creat <name> [-m] [<disk-size-or-path-to-image>] [<path-to-iso-image>]"
-    echo " [<mem-size>] [<nr-of-cpus>] [<internal-network>] [<list-of-forwarded-ports>]"
+    echo "  vm creat <name> [<optional arguments>] - Create new VM '<name>'"
     echo
-    echo " All arguments must be provided in order. The [-m] flag may be omitted, though."
-    echo " Using '-' for a parameter will use its default."
+    echo " Optional arguments include:"
     echo
-    echo "   <name>                        unique identifier for this VM"
-    echo "  [-m]                           Move source disk image and ISO instead of copying."
-    echo "  [<disk-size-or-path-to-image>] Either the size of the harddisk image"
-    echo "                                 (followed by K, M, G or T)"
-    echo "                                 or path to an existing image"
-    echo "                                 (which will be copied). Default: $def_disk"
-    echo "  [<path-to-iso-image>]          Path to an ISO image to use with the VM"
-    echo "  [<mem-size>]                   Amount of memory (followed by M or G). Default: $def_mem"
-    echo "  [<nr-of-cpus>]                 Virtual CPUs count. Default: $def_cpu"
-    echo "  [<internal-network>]           VM-internal network. Default: $def_net"
-    echo "  [<list-of-forwarded-ports>]    List of ports forwarded to host ports,"
-    echo "                                 separated by comma (default '$def_forward_ports')"
+    echo "  -d|--disk <path-to-image>      Copy pre-existing harddisk image instead of creating a new empty volume."
+    echo "  -s|--disk-size <size>          Size of the harddisk volume (may be followed by K, M, G or T)"
+    echo "                                  to be created for the new VM."
+    echo "                                  Default: $disk_size"
+    echo "  -i|--iso <path-to-iso-image>   Path to an ISO image to use with the VM. The image will be copied."
+    echo "  -M|--move                      Move source disk image and ISO instead of copying."
+    echo "  -m|--mem <mem-size>            Amount of memory (followed by M or G)." 
+    echo "                                  Default: $mem"
+    echo "  -c|--cpus <nr-of-cpus>         Virtual CPUs count."
+    echo "                                  Default: $cpus"
+    echo "  -N|--net-mode <hidden|tap>     Networking mode."
+    echo "                 hidden           No host-visible network devices; VM ports need to be forwarded (see -p)."
+    echo "                 tap              VM uses a TAP device on the host. Starting the VM will require root privileges."
+    echo "                                  Default: $net_mode"
+    echo "  -n|--net <internal-network>    VM-internal network (IP/MASK)."
+    echo "                                  Default: $net"
+    echo "  -p|--ports <forwarded-ports>   List of ports forwarded to host ports in 'hidden' network mode,"
+    echo "                                 separated by comma."
+    echo "                                  Default '$forward_ports')"
 }
 # ----
 
 creat_complete() {
     local cword="$1"; shift
-    local words=( $@ )
-    [ "${words[3]-}" = "-m" ] && cword="$((cword-1))"
-    local cur="${words[$cword]-}"
+    [ $cword -le 2 ] && return
 
-    case $cword in
-        3) echo -n "---DEFAULT---" ;;
-        4) echo -n "---DEFAULT---" ;;
+    local words=( $@ )
+    local opts="--iso --disk --disk-size --move --mem --cpus --net-mode --net --ports"
+    local cur="${words[$cword]-}"
+    local prev="${words[$((cword-1))]-}"
+
+
+    # auto-complete options with the default settings
+    case $prev in
+        --iso)          echo "---DEFAULT---";; # complete filenames
+        --disk)         echo "---DEFAULT---";; # complete filenames
+        --disk-size)    echo -n "$disk_size";;
+        --mem)          echo -n "$mem";;
+        --cpus)         echo -n "$cpus";;
+        --net-mode)     echo -n "$net_mode";;
+        --net)          echo -n "$net";;
+        --ports)        echo -n "$forward_ports";;
+        *)              compgen -W "${opts}" -- $cur
     esac
 }
 # ----
 
 image_and_iso() {
-    local op="$1"
-    local disk="$2"
-    local iso="$3"
+    local name="$1"
+    local move="$2"
+    local disk_size="$3"
+    local disk_image="$4"
+    local iso_image="$5"
+
+    local op="cp"
+    $move && op="mv"
 
     [ -e "$vm_path" ] && \
-        die "The path '$vm_path' already exists. Remove it or choose a different name for the VM."
+        die "The path '$vm_path' already exists. Remove it (vm purge ${name}) or choose a different name."
 
     mkdir "$vm_path"
+
     local img="$vm_path/${name}.img"
-    if [ -e "$disk" ] ; then
-        $op -v "$disk" "$img"
+    if [ -e "$disk_image" ] ; then
+        $op -v "$disk_image" "$img"
     else
-        qemu-img create -f raw "$img" "$disk"
+        qemu-img create -f raw "$img" "$disk_size"
     fi
 
     img="$vm_path/${name}.iso"
-    if [ -e "$iso" ] ; then
-        $op -v "$iso" "$img"
+    if [ -e "$iso_image" ] ; then
+        $op -v "$iso_image" "$img"
     fi
 }
 # ----
@@ -92,38 +119,44 @@ write_config() {
 }
 # ----
 
-val_or_def() {
-    local val="$1"
-    local def="$2"
-
-    if [ -z "$val" -o "$val" = "-" ] ; then
-        echo "$def"
-    else
-        echo "$val"
-    fi
-}
-# ----
-
 vm_creat() {
-    local name="$1"
-    local disk_size_or_image=$(val_or_def "${2-}" "${def_disk}")
-    local cp_mv="cp"
-    [ "$disk_size_or_image" = "-m" ] && {
-        cp_mv="mv"
-        shift
-        disk_size_or_image=$(val_or_def "${2-}" "${def_disk}") ; }
-    local iso_image=$(val_or_def "${3-}" "")
-    local mem=$(val_or_def "${4-}" "${def_mem}")
-    local cpu=$(val_or_def "${5-}" "${def_cpu}")
-    local net=$(val_or_def "${6-}" "${def_net}")
-    local ports=$(val_or_def "${7-}" "${def_forward_ports}")
-    image_and_iso "$cp_mv" "$disk_size_or_image" "$iso_image"
+    local opts
+    opts=$(getopt -o i:d:s:Mm:c:N:n:p: \
+                        -l "iso:,disk:,disk-size:,move,mem:,cpus:,net-mode:,net:,ports:" \
+                        -n "vm creat" -- "$@")
+    arg=""; name=""
+    for o in $opts; do
+        # set argument of an option
+        [ -n "$arg" ] && {
+            eval $arg="$o"; arg=""
+            continue
+        }
+
+        # iterate options
+        case $o in
+            -i|--iso)       arg='iso_image';;
+            -d|--disk)      arg='disk_image';;
+            -s|--disk-size) arg='disk_size';;
+            -m|--mem)       arg='mem';;
+            -M|--move)      move=true;;
+            -c|--cpus)      arg='cpus';;
+            -N|--net-mode)  arg='net_mode';;
+            -n|--net)       arg='net';;
+            -p|--ports)     arg='forward_ports';;
+            --)             arg="name";;
+        esac
+    done
+
+    [ -z "$name" ] && usage "This command takes a mandatory <name> argument."
+
+    image_and_iso "$name" "$move" "$disk_size" "$disk_image" "$iso_image"
 
     rm -f "$vm_config"
     write_config "net" "$net"
-    write_config "cpu" "$cpu"
+    write_config "netmode" "$net_mode"
+    write_config "cpu" "$cpus"
     write_config "mem" "$mem"
-    write_config "forward_ports" "${ports//,/ }"
+    write_config "forward_ports" "${forward_ports//,/ }"
 
     echo
     echo " VM $vm_name generated."
